b88684c4eca284139ebab777024a89a176647f08
Силаев Владислав Вячеславович <gr622_sivvya@ttit.local>
2023-11-29 14:46:09 +0700

    fix 1.2

---
+++ Praktikacheskay rabota 1.2/ConsoleApp2.4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Program4
+{
+    class Program4
+    {
+        public static Random _rand = new Random();
+
+        public static float[,] GetRandomFilledMatrixTemperatureYear()
+        {
+            float[,] temperatureYear = new float[12, 30];
+            int[] averageTemperature =
+            {
+                -16, -15, -7,
+                3, 17, 20,
+                23, 20, 11,
+                0, -10, -15
+            };
+            for (int i = 0; i < 12; ++i)
+            {
+                for (int j = 0; j < 30; ++j)
+                {
+                    temperatureYear[i, j] = _rand.Next(averageTemperature[i] - 10, averageTemperature[i] + 10);
+                }
+            }
+            return temperatureYear;
+        }
+        public static float[] GetArrAverageTemperaturesMonths(ref float[,] matrixYear)
+        {
+            float[] averageTemperaturesMonths = new float[12];
+            for (int i = 0; i < 12; ++i)
+            {
+                float averageTemperatureMonth = 0;
+                for (int j = 0; j < 30; ++j)
+                {
+                    averageTemperatureMonth += matrixYear[i, j];
+                }
+                averageTemperaturesMonths[i] = averageTemperatureMonth / 30;
+            }
+            return averageTemperaturesMonths;
+        }
+
+        public static void SortArrFloat(ref float[] arrFloat)
+        {
+            Array.Sort(arrFloat);
+        }
+
+        public static void Main()
+        {
+            float[,] temperatureYear = GetRandomFilledMatrixTemperatureYear();
+            float[] averageTemperaturesMonths = GetArrAverageTemperaturesMonths(ref temperatureYear);
+            SortArrFloat(ref averageTemperaturesMonths);
+            float averageTemperaturesYear = 0;
+
+            Console.WriteLine("Sorted array of average temperatures by month:");
+            foreach (float temperature in averageTemperaturesMonths)
+            {
+                Console.WriteLine($"{(int)temperature}°C"); // Приведение к целому числу для отображения целых чисел
+                averageTemperaturesYear += temperature;
+            }
+
+            Console.WriteLine($"Average temperature for the year: {(int)(averageTemperaturesYear / 12)}°C"); // Приведение к целому числу для отображения целых чисел
+        }
+    }
+}
---
+++ Praktikacheskay rabota 1.2/ConsoleApp2.5/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program5
+{
+    class Program5
+    {
+        public static Random _rand = new Random();
+
+        public static Dictionary<string, int[]> GetRandomFilledTemperatureDictionary()
+        {
+            Dictionary<string, int[]> temperatureYear = new Dictionary<string, int[]>();
+            string[] months = 
+            {
+                "January", "February", "March",
+                "April", "May", "June",
+                "July", "August", "September",
+                "October", "November", "December"
+            };
+
+            int[] averageTemperature = 
+            {
+                -16, -15, -7,
+                3, 17, 20,
+                23, 20, 11,
+                0, -10, -15
+            };
+
+            for (int i = 0; i < 12; i++)
+            {
+                int[] monthTemperatures = new int[30];
+                for (int j = 0; j < 30; j++)
+                {
+                    monthTemperatures[j] = _rand.Next(averageTemperature[i] - 10, averageTemperature[i] + 10);
+                }
+                temperatureYear.Add(months[i], monthTemperatures);
+            }
+
+            return temperatureYear;
+        }
+
+        public static Dictionary<string, int> GetAverageTemperaturePerMonth(Dictionary<string, int[]> temperatureYear)
+        {
+            Dictionary<string, int> averageTemperatures = new Dictionary<string, int>();
+
+            foreach (var month in temperatureYear)
+            {
+                int sum = 0;
+                foreach (var temp in month.Value)
+                {
+                    sum += temp;
+                }
+                int averageTemp = sum / month.Value.Length;
+                averageTemperatures.Add(month.Key, averageTemp);
+            }
+
+            return averageTemperatures;
+        }
+
+        public static void Main()
+        {
+            Dictionary<string, int[]> temperatureYear = GetRandomFilledTemperatureDictionary();
+            
+            Dictionary<string, int> averageTemperatures = GetAverageTemperaturePerMonth(temperatureYear);
+
+            Console.WriteLine("Average temperatures by month:");
+            
+            float averageTemperaturesYear = 0;
+            
+            foreach (var month in averageTemperatures)
+            {
+                Console.WriteLine($"{month.Key}: {month.Value}°C");
+                averageTemperaturesYear += month.Value;
+            }
+            Console.WriteLine($"Average temperature for the year: {(int)(averageTemperaturesYear / 12)}°C");
+        }
+    }
+}
