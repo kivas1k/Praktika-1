9dbcd5a0e449ebff12d118ff1b094d2ee9ef8120
Силаев Владислав Вячеславович <gr622_sivvya@ttit.local>
2023-12-06 18:10:11 +0700

    1.8 fix

--- Praktikacheskay rabota 1.8/Praktikacheskay rabota 1.8/Program.cs
+++ Praktikacheskay rabota 1.8/Praktikacheskay rabota 1.8/Program.cs
@@ -1,67 +1,36 @@
-﻿using Newtonsoft.Json;
-
-namespace WeatherApp
+﻿namespace WeatherApp
 {
-    class Program
+    internal class AttributeWeather
     {
+        public string main { get; set; }
         
-        static async Task Main(string[] args)
-        {
-            WeatherData weatherData = new WeatherData();
-    
-            if (args.Length > 0)
-            {
-                string city = args[0];
-                
-                await weatherData.GetWeather(city);
-            }
-            else
-            {
-                Console.Write("Enter the default city: ");
-                
-                string defaultCity = Console.ReadLine();
-                
-                weatherData.DefaultCity = defaultCity;
-                
-                await weatherData.GetWeather(weatherData.DefaultCity);
-            }
-        }
+        public string description { get; set; }
     }
-    class WeatherData
+
+    internal class AttributeMain
     {
-        public string DefaultCity { get; set; }
+        public float temp { get; set; }
+        
+        public float feelslike { get; set; }
+    }
 
-        public async Task GetWeather(string city)
-        {
-            string apiKey = "56e7fe704b986ccde6b69740196c1f65";
-            
-            string apiUrl = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+    internal class AttributeWind
+    {
+        public float speed { get; set; }
 
-            using (HttpClient client = new HttpClient())
-            {
-                HttpResponseMessage response = await client.GetAsync(apiUrl);
-                
-                if (response.IsSuccessStatusCode)
-                {
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    
-                    WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(responseBody);
+    }
 
-                    Console.WriteLine($"Weather in {city}:");
-                    
-                    Console.WriteLine($"Temperature: {weatherResponse.main.temp}°C");
-                    
-                    Console.WriteLine($"Feels like: {weatherResponse.main.feelslike}°C");
-                    
-                    Console.WriteLine($"Description: {weatherResponse.weather[0].description}");
-                    
-                    Console.WriteLine($"Wind speed: {weatherResponse.wind.speed} m/s");
-                }
-                else
-                {
-                    Console.WriteLine("Failed to retrieve weather data. Please try again.");
-                }
-            }
-        }
+    internal class WeatherResponse
+    {
+        public List<AttributeWeather> weather { get; set; }
+        
+        public AttributeMain main { get; set; }
+        
+        public AttributeWind wind { get; set; }
+    }
+
+    internal class Default
+    {
+
     }
 }
--- Praktikacheskay rabota 1.8/Praktikacheskay rabota 1.8/Program1.cs
+++ Praktikacheskay rabota 1.8/Praktikacheskay rabota 1.8/Program1.cs
@@ -1,31 +1,123 @@
+using Newtonsoft.Json;
+
 namespace WeatherApp
 {
-    internal class AttributeWeather
+    class Program
     {
-        public string main { get; set; }
-        
-        public string description { get; set; }
+        static async Task Main(string[] args)
+        {
+            WeatherData weatherData = new WeatherData();
+
+            if (args.Length > 0)
+            {
+                string city = args[0];
+                
+                await weatherData.GetWeather(city);
+            }
+            else
+            {
+                Console.Write("Enter city: ");
+                
+                string defaultCity = Console.ReadLine();
+                
+                weatherData.DefaultCity = defaultCity;
+                
+                await weatherData.GetWeather(weatherData.DefaultCity);
+            }
+        }
     }
-
-    internal class AttributeMain
+    class WeatherData
     {
-        public float temp { get; set; }
-        
-        public float feelslike { get; set; }
-    }
+        public string path;
 
-    internal class AttributeWind
-    {
-        public float speed { get; set; }
+        public string city;
 
-    }
+        public WeatherData()
+        {
+            path = Directory.GetCurrentDirectory() + "/default.txt";
 
-    internal class WeatherResponse
-    {
-        public List<AttributeWeather> weather { get; set; }
+            city = File.ReadAllText(path);
+
+            if (city.Equals(string.Empty))
+            {
+                Console.WriteLine("Nothing");
+            }
+            else
+            {
+                printDefault(city);
+                Thread.Sleep(900);
+            }
+        }
+
+        public string DefaultCity { get; set; }
+
+        public async Task GetWeather(string city)
+        {
+            string apiKey = "56e7fe704b986ccde6b69740196c1f65";
+            
+            string apiUrl = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+
+            using (HttpClient client = new HttpClient())
+            {
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    
+                    WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(responseBody);
+
+                    Console.WriteLine($"Weather in {city}:");
+                    
+                    Console.WriteLine($"Temperature: {weatherResponse.main.temp}°C");
+                    
+                    Console.WriteLine($"Feels like: {weatherResponse.main.feelslike}°C");
+                    
+                    Console.WriteLine($"Description: {weatherResponse.weather[0].description}");
+                    
+                    Console.WriteLine($"Wind speed: {weatherResponse.wind.speed} m/s");
+                }
+                else
+                {
+                    Console.WriteLine("Failed to retrieve weather data. Please try again.");
+                }
+            }
+        }
+
+        public static async Task printDefault(string city)
+        {
+            string apiKey = "56e7fe704b986ccde6b69740196c1f65";
+            
+            string apiUrl = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+
+            using (HttpClient client = new HttpClient())
+            {
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    
+                    WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(responseBody);
+
+                    Console.WriteLine($"Weather in {city}:");
+                    
+                    Console.WriteLine($"Temperature: {weatherResponse.main.temp}°C");
+                    
+                    Console.WriteLine($"Feels like: {weatherResponse.main.feelslike}°C");
+                    
+                    Console.WriteLine($"Description: {weatherResponse.weather[0].description}");
+                    
+                    Console.WriteLine($"Wind speed: {weatherResponse.wind.speed} m/s");
+                }
+                else
+                {
+                    Console.WriteLine("Failed to retrieve weather data. Please try again.");
+                }
+            }
+        }
         
-        public AttributeMain main { get; set; }
-        
-        public AttributeWind wind { get; set; }
     }
 }
